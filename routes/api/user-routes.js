67d897a9ab99e3be753dffef5015b45a7270a0e0
Ivan Thiessen <ivant920@gmail.com>
2022-06-08 14:44:55 -0400

    Added all the POST routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,7 +3,7 @@ const { User } = require('../../models');
 
 router.get('/', (req, res) => {
     User.findAll({
-        // attributes: { exclude: ['password'] }
+        attributes: { exclude: ['password'] }
     })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
@@ -14,7 +14,7 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
     User.findOne({
-        // attributes: { exclude: ['password'] },
+        attributes: { exclude: ['password'] },
         where: {
             id: req.params.id
         }
